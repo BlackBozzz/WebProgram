3b0742a6b830a0485b5aea7ee28ae56244f9d789
BlackBoss <44169906+BlackBozzz@users.noreply.github.com>
2021-12-18 16:46:20 +0300

    Add files via upload

--- Seminar11-12.js
+++ Seminar11-12.js
@@ -3,12 +3,18 @@ let city1 = {};
 city1.name = "ГородN";
 city1.population = 10e6;
 console.log(city1);
+
+
+
 //2
 let city2 = {
     name: "ГородM",
     population: 1e6
 };
 console.log(city2);
+
+
+
 //3
 city1.getName = function () {
     return this["name"];
@@ -18,6 +24,9 @@ city2.getName = function () {
 };
 console.log(city1.getName());
 console.log(city2.getName());
+
+
+
 //4
 city1.exportStr = function () {
     let str = "";
@@ -42,6 +51,9 @@ city2.exportStr = function () {
 };
 console.log(city1.exportStr());
 console.log(city2.exportStr());
+
+
+
 //5
 function getObj() {
     return this;
@@ -50,6 +62,9 @@ city1.getCity = getObj;
 city2.getCity = getObj;
 console.log(city1.getCity());
 console.log(city2.getCity());
+
+
+
 //6
 let obj = {};
 obj.method3 = function () {
@@ -62,10 +77,16 @@ obj.method1 = function () {
     return this;
 };
 console.log(obj.method1().method2().method3());
+
+
+
 //7
 let d1 = [45, 78, 10, 3];
 d1[7] = 100;
 console.log(d1, d1[6], d1[7]);
+
+
+
 //8
 let d2 = [45, 78, 10, 3];
 let sum2 = 0;
@@ -73,6 +94,9 @@ for (let i= 0; i < d2.length; i++) {
     sum2 += d2[i];
 }
 console.log(sum2);
+
+
+
 //9
 let d3 = [45, 78, 10, 3];
 d3[7] = 100;
@@ -81,12 +105,18 @@ for (let index in d3) {
     sum3 += d3[index];
 }
 console.log(sum3);
+
+
+
 //10
 let d4 = [45, 78, 10, 3];
 function my(a, b) {
     return a-b;
 }
 console.log(d4.sort(my));
+
+
+
 //11
 let d5 = [];
 let n = 3;
@@ -98,6 +128,33 @@ for (let i = 0; i < n; i++) {
     }
 }
 console.log(d5);
+
+
+
+
+//12
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function(vector2) {
+    return new Vector(this.x + vector2.x, this.y + vector2.y);
+};
+Vector.prototype.minus = function(vector2) {
+    return new Vector(this.x - vector2.x, this.y - vector2.y);
+};
+Object.defineProperty(Vector.prototype, 'length', {
+    get() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+});
+console.log(new Vector(1,2).plus(new Vector(2,3)));
+console.log(new Vector(1,2).minus(new Vector(2,3)));
+console.log(new Vector(3,4).length);
+
+
+
+
 //15
 function Card(from, to) {
     this.from = from;
@@ -108,6 +165,11 @@ function Card(from, to) {
 }
 let c1 = new Card("Екатеринбург", "Москва");
 console.log(c1.show());
+
+
+
+
+
 //16
 class Human {
     constructor(name, age, height) {
@@ -140,6 +202,10 @@ for (let i = 0; i < humans.length; i++) {
 console.log(humans);
 console.log(humans[0].getInfo());
 console.log(humans[0].firstname);
+
+
+
+
 //17
 function sortByName(humans) {
     for (let i = 0; i < humans.length - 1; i++) {
@@ -166,14 +232,81 @@ function sortByHeight(humans) {
     return humans;
 }
 console.log(sortByHeight(humans));
+
+
+
+
+
 //18
 let dt1 = new Date(2045, 0, 1, 0,0);
 console.log(dt1);
+
+
+
+
 //19
 let dt2 = new Date()/1000;
 console.log(dt2);
+
+
+
+
 //20
 function getDays(year, month) {
     return new Date(year, month, 0).getDate();
 }
 console.log(getDays(2021, 2));
+
+
+
+
+//21
+function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+    let day = date.getDay();
+    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    return day - 1;
+}
+
+function createCalendar(elem, year, month) {
+
+    let mon = month - 1;
+    let d = new Date(year, mon);
+
+    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+    for (let i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
+    }
+
+    while (d.getMonth() == mon) {
+        table += '<td>' + d.getDate() + '</td>';
+
+        if (getDay(d) % 7 == 6) {
+            table += '</tr><tr>';
+        }
+
+        d.setDate(d.getDate() + 1);
+    }
+
+    if (getDay(d) != 0) {
+        for (let i = getDay(d); i < 7; i++) {
+            table += '<td></td>';
+        }
+    }
+    
+    table += '</tr></table>';
+
+    elem.innerHTML = table;
+}
+createCalendar(document.getElementById("calendar"), 2021, 12)
+
+
+
+
+//22
+Object.defineProperty(Number.prototype, 'isOdd', {
+    get() {
+        return 1 == this.valueOf()%2;
+    }
+});
+console.log(new Number(5).isOdd)
